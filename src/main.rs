0e30370effd548f1c611e00b31252460b7e911a4
Chetan <chetanrjj2002@gmail.com>
2025-03-07 17:27:42 +0000

    modularized the code by moving it into lib.rs and also fixed the projects name type to greprs

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+extern crate greprs;
+use greprs::Config;
+
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
 fn main() {
     let args:Vec<String> = env::args().collect();
 
@@ -10,32 +11,8 @@ fn main() {
         process::exit(1);
     });
     
-    if let Err(e) = run(config){ // not using unwrap_or_else cuz run returns a () type
+    if let Err(e) = greprs::run(config){ // not using unwrap_or_else cuz run returns a () type
         println!("Application error: {}", e);
         process::exit(1);
     };
-}
-
-struct Config {
-    query:String,
-    filename:String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Self, &str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-    
-        Ok(Self {query, filename})
-        
-    }   
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>>{
-    let file_content = fs::read_to_string(config.filename)?;
-    println!("the file content is: {}", file_content);
-    Ok(())
 }
