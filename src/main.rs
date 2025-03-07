14646fe4cf426ae6b101e0ef57bc4cce8e3bb4b1
Chetan <chetanrjj2002@gmail.com>
2025-03-07 16:45:19 +0000

    added some simple error handeling

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 use std::env;
 use std::fs;
+use std::process;
 fn main() {
     let args:Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = Config::new(&args).unwrap_or_else(|e| {
+        println!("problem parsing arguments: {}", e);
+        process::exit(1);
+    });
     
     let file_content = fs::read_to_string(config.filename).expect("file not found");
 
@@ -16,10 +20,14 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Self {
+    fn new(args: &[String]) -> Result<Self, &str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
     
-        Self {query, filename}
+        Ok(Self {query, filename})
+        
     }   
 }
