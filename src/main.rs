bea2bcba0bde077081523f7f07ce599bb11dcb80
Chetan <chetanrjj2002@gmail.com>
2025-03-07 16:32:32 +0000

    refactored a bit

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,23 @@ use std::fs;
 fn main() {
     let args:Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
     
-    let file_content = fs::read_to_string(filename).expect("the file doesn't exist in the current directory");
+    let file_content = fs::read_to_string(config.filename).expect("file not found");
 
     println!("the file content is: {}", file_content);
+}
+
+struct Config {
+    query:String,
+    filename:String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Self {
+        let query = args[1].clone();
+        let filename = args[2].clone();
+    
+        Self {query, filename}
+    }   
 }
