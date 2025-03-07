bcafa71446533d16c0e11d07219fb329fadb0550
Chetan <chetanrjj2002@gmail.com>
2025-03-07 12:56:57 +0000

    reads the user argument from command

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
 use std::env;
-
+use std::fs;
 fn main() {
+    let args:Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let filename = &args[2];
     
+    let file_content = fs::read_to_string(filename).expect("the file doesn't exist in the current directory");
+
+    println!("the file content is: {}", file_content);
 }
