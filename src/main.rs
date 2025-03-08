58c76b0b13396bfd4586ad23fa737ea1237f6e5b
Chetan <chetanrjj2002@gmail.com>
2025-03-08 12:16:03 +0000

    errors now print into stderr instead of standard output

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,23 @@ use greprs::Config;
 
 use std::env;
 use std::process;
+use std::io::prelude::*;
 fn main() {
     let args:Vec<String> = env::args().collect();
+    let mut stderr = std::io::stderr();
 
     let config = Config::new(&args).unwrap_or_else(|e| {
-        println!("problem parsing arguments: {}", e);
+        writeln!(
+            &mut stderr, 
+            "problem parsing arguments: {}", e
+        ).expect("could not write to stderr");
         process::exit(1);
     });
     
     if let Err(e) = greprs::run(config){ // not using unwrap_or_else cuz run returns a () type
-        println!("Application error: {}", e);
+        writeln!(
+            &mut stderr, "Application error: {}", e
+        ).expect("could not write to stderr");
         process::exit(1);
     };
 }
